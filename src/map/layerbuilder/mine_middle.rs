83c48bef1ff1049b61f6b0d1b35b1a52a3fce959
Herbert Wolverson <herberticus@gmail.com>
2021-03-07 16:35:04 -0600

    Work in progress - drunkard mining for the mine middle.

--- src/map/layerbuilder/mine_middle.rs
+++ src/map/layerbuilder/mine_middle.rs
@@ -1,8 +1,5 @@
 use super::{all_wall, colonists::spawn_first_colonist, spawn_face_eater, spawn_random_colonist};
-use crate::{
-    components::{Description, Door, Glyph, Position, TileTrigger},
-    map::{tile::TileType, Layer, Tile, HEIGHT, WIDTH},
-};
+use crate::{components::{Description, Door, Glyph, Position, TileTrigger}, map::{HEIGHT, Layer, TILES, Tile, WIDTH, tile::TileType}};
 use bracket_lib::prelude::*;
 use legion::*;
 
@@ -38,7 +35,52 @@ pub fn build_mine_middle(ecs: &mut World) -> Layer {
     layer.starting_point = up_pt;
     layer.colonist_exit = down_pt;
 
-    // Start building rooms and corridors
+    // Start using drunkard's walk to dig outwards
+    while layer.tiles
+        .iter()
+        .filter(|t| t.tile_type == TileType::Floor).count() < TILES / 3
+    {
+        drunkard(&mut layer);
+    }
 
     layer
+}
+
+fn drunkard(map: &mut Layer) {
+    let mut rng_lock = crate::RNG.lock();
+    let rng = rng_lock.as_mut().unwrap();
+
+    let possible_starts : Vec<usize> = map
+        .tiles
+        .iter()
+        .enumerate()
+        .filter(|(_, t)| t.tile_type == TileType::Floor)
+        .map(|(i,_)| i)
+        .collect();
+
+    let start = rng.random_slice_entry(&possible_starts).unwrap();
+    let mut drunkard_pos = map.index_to_point2d(*start);
+    let mut distance_staggered = 0;
+
+    loop {
+        let drunk_idx = map.point2d_to_index(drunkard_pos);
+        if map.tiles[drunk_idx].tile_type == TileType::Wall {
+            map.tiles[drunk_idx] = Tile::floor();
+        }
+
+        match rng.range(0, 4) {
+            0 => drunkard_pos.x -= 1,
+            1 => drunkard_pos.x += 1,
+            2 => drunkard_pos.y -= 1,
+            _ => drunkard_pos.y += 1,
+        }
+        if !map.in_bounds(drunkard_pos) {
+            break;
+        }
+
+        distance_staggered += 1;
+        if distance_staggered > 200 {
+            break;
+        }
+    }
 }
