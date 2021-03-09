0e5e338b00225a6ef0e700190b0741b93be70169
Herbert Wolverson <herberticus@gmail.com>
2021-03-09 11:32:26 -0600

    Cleanup font linkage.

--- src/main.rs
+++ src/main.rs
@@ -12,27 +12,31 @@ mod turn;
 pub use turn::NewState;
 use turn::State;
 
-embedded_resource!(TILE_FONT, "../resources/font-16x16.png");
-
 pub const LAYER_MAP: usize = 0;
 pub const LAYER_DECOR: usize = 1;
 pub const LAYER_ITEMS: usize = 2;
 pub const LAYER_CHR: usize = 3;
 pub const LAYER_TEXT: usize = 4;
 
+embedded_resource!(TILE_FONT, "../resources/font.png");
+embedded_resource!(VGA_FONT, "../resources/vga.png");
+
 fn main() -> BError {
+    link_resource!(TILE_FONT, "resources/font.png");
+    link_resource!(VGA_FONT, "resources/vga.png");
+
     let context = BTermBuilder::new()
         .with_title("Secbot - 2021 7DRL") // Set Window Title
         .with_tile_dimensions(16, 16) // Calculate window size with this...
         .with_dimensions(56, 31) // ..Assuming a console of this size
         .with_fps_cap(60.0) // Limit game speed
-        .with_font("font-16x16.png", 16, 16) // Load big font
-        .with_font("vga8x16.png", 8, 16) // Load easy-to-read font
-        .with_simple_console(56, 31, "font-16x16.png") // Console 0: Base map
-        .with_sparse_console(56, 31, "font-16x16.png") // Console 1: Decorations
-        .with_sparse_console(56, 31, "font-16x16.png") // Console 2: Items
-        .with_sparse_console(56, 31, "font-16x16.png") // Console 3: Characters
-        .with_sparse_console(112, 31, "vga8x16.png") // Console 4: User Interface
+        .with_font("font.png", 16, 16) // Load big font
+        .with_font("vga.png", 8, 16) // Load easy-to-read font
+        .with_simple_console(56, 31, "font.png") // Console 0: Base map
+        .with_sparse_console_no_bg(56, 31, "font.png") // Console 1: Decorations
+        .with_sparse_console_no_bg(56, 31, "font.png") // Console 2: Items
+        .with_sparse_console_no_bg(56, 31, "font.png") // Console 3: Characters
+        .with_sparse_console(112, 31, "vga.png") // Console 4: User Interface
         .build()?;
 
     main_loop(context, State::new())
